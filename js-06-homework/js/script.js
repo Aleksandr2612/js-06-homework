5f3eb267caddab4c21c99d2a55d07072648c766a
Aleksandr2612 <101992736+Aleksandr2612@users.noreply.github.com>
2022-06-17 23:01:48 +0300

    Add files via upload

---
+++ js-06-homework/js/script.js
@@ -0,0 +1,68 @@
+// №1
+// let harryPotterTeam = ["Albus", "Hermione", "Ronald", "Draco", "Neville"]
+
+// let teamA = [];
+// let teamH = [];
+// harryPotterTeam.forEach(item => {
+//     if (item.includes('a')) {
+//         teamA.push(item);
+//         console.log(teamA);
+//     } else if (item.includes('H')) {
+//         teamH.push(item);
+//         console.log(teamH);
+//     }
+// });
+
+
+// №2
+
+// let salary = [6, 3, true, "4", 1];
+// let sum = 0;
+// let arr = [];
+// salary.forEach(item => {
+//     if (item > 0 && item !== "strimg") {
+//         sum += +item;
+
+//     }
+// });
+// console.log(sum);
+
+// №3 
+
+// numArr = [1, 3, 5, 7, 9, 11]
+
+// let arr = [];
+// numArr.forEach(item => {
+//     item *= item;
+//     arr.push(item);
+// });
+// console.log(arr);
+
+// №4
+// let students = [
+//     { name: 'Alexey', id: 123, marks: 9 }, { name: 'Vitalik', id: 101, marks: 5 },
+//     { name: 'Tanya', id: 200, marks: 7 },
+//     { name: 'Sasha', id: 115, marks: 10 }
+// ]
+
+// let resultId = students.find(function(item) {
+//     return item.id === 101;
+// });
+// console.log(resultId);
+
+// №5
+
+// let javaScriptTypes = ["number", "null", "undefined", "string"];
+// console.log(javaScriptTypes);
+
+// let allJsTyps = javaScriptTypes.concat("bigint", "boolean", "object", "symbol");
+// console.log(allJsTyps);
+
+// №6
+
+// let parfume = ["Ex Nihilo", "Killian", "BDK", "Replica"]
+
+// console.log(parfume);
+
+// parfume[3] = 'Jo Malone';
+// console.log(parfume);
